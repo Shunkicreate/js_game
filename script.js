705675ec34d2c33e5982f84d542ddfba5fc91cef
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2022-02-17 19:41:01 +0900

    fin gravity

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ window.addEventListener('load', function () {
             });
             window.addEventListener('keyup', e => {
                 if (e.key === 'ArrowDown' ||
-                        e.key === 'ArrowUp' ||
-                        e.key === 'ArrowLeft' ||
-                        e.key === 'ArrowRight' ) {
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight') {
                     // this.keys.splice(e.key.indexOf(e.key), 1);
-                    this.keys.splice(0,this.keys.length);
+                    this.keys.splice(0, this.keys.length);
                 }
                 console.log(this.keys)
             });
@@ -31,23 +31,61 @@ window.addEventListener('load', function () {
 
     }
     class Player {
-        constructor(gameWidth, gameHeight){
-            this.gamewidth = gameWidth;
-            this.gameheight = gameHeight;
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
             this.width = 200;
             this.height = 200;
             this.x = 0;
-            this.y = 0;
+            this.y = this.gameHeight - this.height;
+            this.image = document.getElementById('playerImage');
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 1;
+            this.vy = 0;
+            this.weight = 1;
+
+
         }
 
-        draw(context){
+        draw(context) {
             context.fillStyle = 'white';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            // context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 
         }
-        update(){
-            this.x++;
-            this.y++
+        update(input) {
+            this.x += this.speed;
+            if (input.keys.indexOf('ArrowRight') > -1) {
+                this.speed = 5;
+            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                this.speed = -5
+            } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+                this.vy -= 30;
+            } else {
+                this.speed = 0;
+            }
+            //水平方向の動き
+            this.x += this.speed;
+
+            if(this.x < 0) this.x = 0;
+            else if(this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            // if (this.x < 0 || this.x > this.gameWidth - this.width) {
+            //     this.speed = -this.speed;
+            //     this.x += (this.speed) * 3;
+            //     console.log(this.speed);
+            // }
+
+            //垂直方向の動き
+            this.y += this.vy;
+            if (!this.onGround()) {
+                this.vy += this.weight;
+                this.frameY = 0;
+            } 
+            if(this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        }
+        onGround() {
+            return this.y >= this.gameHeight - this.height;
         }
 
     }
@@ -71,9 +109,11 @@ window.addEventListener('load', function () {
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
     
+
     function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         player.draw(ctx);
-        player.update();
+        player.update(input);
         requestAnimationFrame(animate);
 
     }
